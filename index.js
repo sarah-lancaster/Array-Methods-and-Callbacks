9f3dac0b1d987e3f80338f170783f51ddeafe7dd
TylerOliverTaylor <61921198+TylerOliverTaylor@users.noreply.github.com>
2020-06-06 23:31:35 -0400

    task2

--- index.js
+++ index.js
@@ -97,15 +97,18 @@ console.log(finalWinCondition);
 /* Task 2: Create a function called  getFinals that takes `data` as an argument 
 and returns an array of objects with only finals data */
 
-// function getFinals(data) {
-//     let data = 
-//     const finalsOnly = data.filter((stage) => {
-//         return stage.stage = "Final";
-//     })
-// };
+function getFinals(data) {
+   const finalsOnly = [];
+   for (let i=0; i <data.length; i++) {
+       if (data[i].Stage === "Final") {
+           finalsOnly.push(data[i]);
+       };
+   };
+console.log(finalsOnly);
 
-// getFinals();
+}
 
+getFinals(fifaData);
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts 
 the callback function `getFinals`, and returns an array called `years` containing 
